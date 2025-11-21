b6005f2e5357f256e9a4fe609fef90eb45c8d587
Meltingwax-19 <142224149+meltingwax-19@users.noreply.github.com>
2025-11-21 12:37:13 +0100

    final version

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header,
+    Hero,
+    About,
+    Skills,
+    Portfolio,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('ng-portfolio-lonely');
